f0debb305a5a048344b29039505d6c110b7a8e40
TSlash <45912519+TSlashDreamy@users.noreply.github.com>
2023-12-09 18:49:01 +0200

    implemented thunks

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,5 +1,22 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../store";
+
 const UsersList = () => {
-  return <div>users</div>;
+  const dispath = useDispatch();
+  const { data, isLoading, error } = useSelector((state) => state.users);
+
+  useEffect(() => {
+    dispath(fetchUsers());
+  }, [dispath]);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) {
+    console.error(error);
+    return <div>Fetching error. (Check the console)</div>;
+  }
+
+  return <div>{data.length}</div>;
 };
 
 export default UsersList;
