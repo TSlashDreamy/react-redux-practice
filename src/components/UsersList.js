c12b1b3bbfaa243c94e61b6f8146d5bac4d8b7e7
TSlash <45912519+TSlashDreamy@users.noreply.github.com>
2023-12-21 20:38:02 +0200

    userlist refactor

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -4,48 +4,38 @@ import { addUser, fetchUsers } from "../store";
 import Skeleton from "./Skeleton";
 import Button from "./Button";
 import useThunk from "../hooks/use-thunk";
+import UsersListItem from "./UsersListItem";
 
 const UsersList = () => {
   const [doLoadUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
   const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
   const { data } = useSelector((state) => state.users);
+  let content;
 
   useEffect(() => {
     doLoadUsers();
   }, [doLoadUsers]);
 
-  useEffect(() => {
-    console.log(isCreatingUser, isLoadingUsers);
-  }, [isCreatingUser, isLoadingUsers]);
-
   const handleUserAdd = () => {
     doCreateUser();
   };
 
-  if (isLoadingUsers) return <Skeleton times={6} className="h-10 w-full" />;
-  if (loadingUsersError) {
-    console.error(loadingUsersError);
-    return <div>Fetching error. (Check the console)</div>;
+  if (isLoadingUsers) content = <Skeleton times={6} className="h-10 w-full" />;
+  else if (loadingUsersError) content = <div>Fetching error ⚠️</div>;
+  else {
+    content = data.map((user) => <UsersListItem key={user.id} user={user} />);
   }
-
-  const renderedUsers = data.map((user) => (
-    <div key={user.id} className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        {user.name}
-      </div>
-    </div>
-  ));
 
   return (
     <div>
-      <div className="flex flex-row justify-between m-3">
+      <div className="flex flex-row justify-between items-center m-3">
         <h1 className="m-2 text-xl">Users</h1>
         <Button loading={isCreatingUser} onClick={handleUserAdd}>
           + Add User
         </Button>
         {creatingUserError && "Create user error!"}
       </div>
-      {renderedUsers}
+      {content}
     </div>
   );
 };
